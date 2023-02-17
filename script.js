c801a738a7816ea8544a3ede68de14419d73caeb
AmadeAnjos <anjosbarrosclean@gmail.com>
2023-02-16 21:56:04 -0300

    mexendo no javascript

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const nome = document.querySelector('#name')
+const tel = document.querySelector('#phone')
+const email = document.querySelector('#city')
+const action = document.querySelector('.acao')
+
+let itens
+let id
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbclient')) ?? []
+const setItensBD = () => localStorage.setItem('dbclient', JSON.stringify(itens))
 
 function abrirModal(edit = false, index = 0) {
     modal.classList.add('active')
@@ -7,4 +18,9 @@ function abrirModal(edit = false, index = 0) {
 function fecharModal(edit = false, index = 0) {
     modal.classList.remove('active')
     alert('Cadastrado com sucesso !')
+}
+
+loadItens() {
+    items = getItensBD()
+    tbody.innerHTML = ''
 }
